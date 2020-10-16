bcec4c518878b30441e2a77a64fe715612aeaa97
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-16 14:44:27 +0000

    Deploy dsccodecollab/dsccodecollab.github.io to github.com/dsccodecollab/dsccodecollab.github.io.git:gh-pages

---
+++ precache-manifest.147ba6b261b96ff6ada69ca1d05127d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d13a722882a813b13cb83c9198ff27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70ed4f7d5c17f259581",
+    "url": "/static/css/main.39f5c3ab.chunk.css"
+  },
+  {
+    "revision": "0a0eb8db9e27960eb91b",
+    "url": "/static/js/2.ed0e857e.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/static/js/2.ed0e857e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c70ed4f7d5c17f259581",
+    "url": "/static/js/main.8534e5b8.chunk.js"
+  },
+  {
+    "revision": "de8ef5f1979964cee48e",
+    "url": "/static/js/runtime-main.2d42c579.js"
+  }
+]);
